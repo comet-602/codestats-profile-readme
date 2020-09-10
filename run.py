721379e2f0cbfa7253c9544fb77544d1db5a7b98
comet-602 <connecting941@gmail.com>
2020-09-10 13:29:09 +0800

    make it better

--- run.py
+++ run.py
@@ -4,4 +4,4 @@ import os
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=int(os.environ("host",88)))
+    app.run(host='0.0.0.0',port=int(os.environ.get("host",88)))
